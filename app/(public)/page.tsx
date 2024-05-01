9c6160b7bf7a55ba5675883a1462b047507279b9
Gilbert Owen <88517849+GilbertOwen@users.noreply.github.com>
2024-05-01 11:29:28 +0700

    Created app introduce component (#5)
    * feat: created hero component for public page
    
    * feat: added custom media breakpoint for reponsive layout
    
    * refactor: removed unecessary style
    
    * feat: added overflow hidden to body
    
    * feat: created home page  and added hero component to home public page
    
    * feat: created sidebar component layout
    
    * style: changed the font color style and animation
    
    * refactor: changed the title and description about the app
    
    * feat: added Introduce component to public home page
    
    * fix: fixed bad spacing between component
    
    * feat: created Introduce component and its content
    
    * feat: added resource image for Introduce component content
    
    * fix build error
    
    * fix build error
    
    * fix build error
    
    ---------
    
    Co-authored-by: Gilbert Owen <moptius@gmail.com>

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Hero from "./_components/Hero";
+import Introduce from "./_components/Introduce";
 
 export default async function HomePage() {
   return (
     <Fragment>
       <Hero></Hero>
+      <Introduce></Introduce>
     </Fragment>
   );
 }
