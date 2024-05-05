10bfab377d943fc42edc5c076579cdc30595e56a
Gilbert Owen <88517849+GilbertOwen@users.noreply.github.com>
2024-05-05 21:10:01 +0700

    Fixex animation on introduce component (#12)
    * feat: created hero component for public page
    
    * feat: added custom media breakpoint for reponsive layout
    
    * refactor: removed unecessary style
    
    * feat: added overflow hidden to body
    
    * feat: created home page  and added hero component to home public page
    
    * feat: created sidebar component layout
    
    * style: changed the font color style and animation
    
    * refactor: changed the title and description about the app
    
    * feat: added Introduce component to public home page
    
    * fix: fixed bad spacing between component
    
    * feat: created Introduce component and its content
    
    * feat: added resource image for Introduce component content
    
    * fix build error
    
    * fix build error
    
    * fix build error
    
    * feat: added react icon depedencies
    
    * feat: added keyframes boing animation to tailwind config
    
    * feat: created Benefits component
    
    * feat: added Benefits component to public home page
    
    * feat: added scroll snap to public home page
    
    * style: redesign Hero component background and the other
    
    * feat: added scroll smooth behavior to html
    
    * style: restyling components on public home page
    
    * feat: added gsap depedencies
    
    * feat: added animation to hero component
    
    * feat: added framer-motion depedencies to the app
    
    * fix: fixed animation bugs
    
    * feat: added overflow hidden so the page wont overflow
    
    * feat: added animation to introduce component
    
    * fix: fixed animation bug
    
    ---------
    
    Co-authored-by: Gilbert Owen <moptius@gmail.com>

--- app/(public)/_components/Introduce.tsx
+++ app/(public)/_components/Introduce.tsx
@@ -12,13 +12,13 @@ export default function Introduce() {
   return (
     <section
       id="introduce"
-      className="h-screen lg:h-fit justify-center w-full lg:py-40 xl:py-24 snap-always snap-center flex flex-col gap-y-10 md:gap-y-20"
+      className="h-screen lg:h-fit justify-center w-full lg:py-40 lg:mb-10 mb-0 xl:py-24 snap-always snap-center flex flex-col gap-y-10 md:gap-y-20"
     >
       <motion.h1
         initial={{
           opacity: 0,
           y: "-100%",
-          scaleX:0
+          scaleX: 0,
         }}
         whileInView={{
           opacity: 1,
@@ -33,37 +33,25 @@ export default function Introduce() {
       >
         {introduce.title}
       </motion.h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 place-content-center gap-y-4">
-        <motion.div
-          initial={{
-            opacity: 0,
-            x: "-100%",
-          }}
-          whileInView={{
-            opacity: 1,
-            x: 0,
-            transition: {
-              delay: 1,
-              duration: 0.8,
-            },
-          }}
-          className="md:pl-6"
-        >
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 50,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 1,
+            duration: 0.6,
+          },
+        }}
+        className="grid grid-cols-1 md:grid-cols-2 place-content-center gap-y-4"
+      >
+        <div className="md:pl-6">
           <img src="/easy-maintain.png" alt="" />
-        </motion.div>
-        <motion.div
-          initial={{
-            opacity: 0,
-            x:'100%',
-          }}
-          whileInView={{
-            opacity: 1,
-            x: 0,
-            transition: {
-              delay: 1.8,
-              duration: 0.6,
-            },
-          }}
+        </div>
+        <div
           className="relative ml-auto md:mt-4 group md:hover:w-[100%] lg:mt-14 md:rounded-l-full transition-all flex md:pl-20 pl-4 pr-4 py-8 md:py-4 md:pr-4 flex-col justify-center items-center w-full md:w-[90%] lg:w-[80%] h-[80%] bg-[#DAC0A3]"
         >
           <span className="md:group-hover:opacity-100 opacity-0 text-5xl font-semibold text-[#102C57] absolute top-10 transition-all">
@@ -72,8 +60,8 @@ export default function Introduce() {
           <p className="font-semibold text-[#102C57] text-center text-lg lg:text-2xl xl:text-3xl">
             {introduce.description}
           </p>
-        </motion.div>
-      </div>
+        </div>
+      </motion.div>
     </section>
   );
 }
