278d7cceba8ae0e822947e0fde5ef46b188d945d
Gilbert Owen <88517849+GilbertOwen@users.noreply.github.com>
2024-05-05 22:03:55 +0700

    Upgraded benefits component (#13)
    * feat: created hero component for public page
    
    * feat: added custom media breakpoint for reponsive layout
    
    * refactor: removed unecessary style
    
    * feat: added overflow hidden to body
    
    * feat: created home page  and added hero component to home public page
    
    * feat: created sidebar component layout
    
    * style: changed the font color style and animation
    
    * refactor: changed the title and description about the app
    
    * feat: added Introduce component to public home page
    
    * fix: fixed bad spacing between component
    
    * feat: created Introduce component and its content
    
    * feat: added resource image for Introduce component content
    
    * fix build error
    
    * fix build error
    
    * fix build error
    
    * feat: added react icon depedencies
    
    * feat: added keyframes boing animation to tailwind config
    
    * feat: created Benefits component
    
    * feat: added Benefits component to public home page
    
    * feat: added scroll snap to public home page
    
    * style: redesign Hero component background and the other
    
    * feat: added scroll smooth behavior to html
    
    * style: restyling components on public home page
    
    * feat: added gsap depedencies
    
    * feat: added animation to hero component
    
    * feat: added framer-motion depedencies to the app
    
    * fix: fixed animation bugs
    
    * feat: added overflow hidden so the page wont overflow
    
    * feat: added animation to introduce component
    
    * fix: fixed animation bug
    
    * feat: added animation to benefits section
    
    * refactor: changed the animation delay and duration
    
    ---------
    
    Co-authored-by: Gilbert Owen <moptius@gmail.com>

--- app/(public)/_components/Benefits.tsx
+++ app/(public)/_components/Benefits.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { motion } from "framer-motion";
 import { useState } from "react";
 import { AiFillSafetyCertificate, AiOutlineSafety } from "react-icons/ai";
 import { BsAlarmFill } from "react-icons/bs";
@@ -8,23 +9,43 @@ export default function Benefit() {
   const benefits = [
     {
       title: "Easy",
-      icon: <FiTarget color="#DAC0A3" className="w-[40px] md:w-[80px] lg:w-[120px] transition-all xl:w-[180px] h-[auto]" />,
+      icon: (
+        <FiTarget
+          color="#DAC0A3"
+          className="w-[40px] md:w-[80px] lg:w-[120px] transition-all xl:w-[180px] h-[auto]"
+        />
+      ),
       description: "Achieve tasks effortlessly with our intuitive solutions.",
     },
     {
       title: "Fantastic!",
-      icon: <FiStar color="#DAC0A3" className="w-[40px] md:w-[80px] lg:w-[120px] transition-all xl:w-[180px] h-[auto]" />,
+      icon: (
+        <FiStar
+          color="#DAC0A3"
+          className="w-[40px] md:w-[80px] lg:w-[120px] transition-all xl:w-[180px] h-[auto]"
+        />
+      ),
       description:
         "Experience excellence in every aspect with our amazing services.",
     },
     {
       title: "Discipline",
-      icon: <BsAlarmFill color="#DAC0A3" className="w-[40px] md:w-[80px] lg:w-[120px] transition-all xl:w-[180px] h-[auto]" />,
+      icon: (
+        <BsAlarmFill
+          color="#DAC0A3"
+          className="w-[40px] md:w-[80px] lg:w-[120px] transition-all xl:w-[180px] h-[auto]"
+        />
+      ),
       description: "Stay organized and on track with our structured approach.",
     },
     {
       title: "Safe",
-      icon: <AiOutlineSafety color="#DAC0A3" className="w-[40px] md:w-[80px] lg:w-[120px] transition-all xl:w-[180px] h-[auto]" />,
+      icon: (
+        <AiOutlineSafety
+          color="#DAC0A3"
+          className="w-[40px] md:w-[80px] lg:w-[120px] transition-all xl:w-[180px] h-[auto]"
+        />
+      ),
       description:
         "Ensure security and peace of mind with our reliable measures.",
     },
@@ -41,13 +62,44 @@ export default function Benefit() {
       id="benefits"
       className="snap-always snap-center h-screen justify-center lg:h-fit w-full pt-12 gap-y-4 md:gap-y-10 pb-24 px-6 md:px-10 lg:px-20 flex flex-col items-center"
     >
-      {/* <div className=" gap-y-4 flex flex-col"> */}
-      <h1 className="text-3xl sm:text-4xl md:text-5xl text-center font-semibold text-[#102C57]">
+      <motion.h1
+        initial={{
+          opacity: 0,
+          y: 50,
+          scaleX: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+          scaleX: 1,
+          y: 0,
+          transition: {
+            delay: 0.4,
+            duration: 0.6,
+          },
+        }}
+        className="text-3xl sm:text-4xl md:text-5xl text-center font-semibold text-[#102C57]"
+      >
         Good Benefits
-      </h1>
-      {/* <p className="text-xl font-medium">These are the benefits that you would get if using our app</p> */}
-      {/* </div> */}
-      <div className="grid grid-cols-2 md:grid-cols-4 py-10 w-full md:w-fit gap-y-4 md:gap-y-10 gap-x-4 md:gap-x-10">
+      </motion.h1>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 50,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 1,
+            duration: 0.6,
+          },
+        }}
+        exit={{
+          opacity: 0,
+          y: 100,
+        }}
+        className="grid grid-cols-2 md:grid-cols-4 py-10 w-full md:w-fit gap-y-4 md:gap-y-10 gap-x-4 md:gap-x-10"
+      >
         {benefits &&
           benefits.map((benefit, index) => (
             <div
@@ -68,13 +120,27 @@ export default function Benefit() {
               </div>
             </div>
           ))}
-      </div>
-      <div className="flex flex-col items-center gap-y-4 text-[#DAC0A3]  text-center">
+      </motion.div>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 50,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 1.4,
+            duration: 0.6,
+          },
+        }}
+        className="flex flex-col items-center gap-y-4 text-[#DAC0A3]  text-center"
+      >
         <h2 className="text-3xl font-semibold capitalize">
           {clickedBenefit.title}
         </h2>
         <p>{clickedBenefit.description}</p>
-      </div>
+      </motion.div>
     </section>
   );
 }
--- app/(public)/_components/Introduce.tsx
+++ app/(public)/_components/Introduce.tsx
@@ -25,7 +25,7 @@ export default function Introduce() {
           scaleX: 1,
           y: 0,
           transition: {
-            delay: 0.4,
+            delay:0.2,
             duration: 0.6,
           },
         }}
@@ -42,7 +42,7 @@ export default function Introduce() {
           opacity: 1,
           y: 0,
           transition: {
-            delay: 1,
+            delay: 0.6,
             duration: 0.6,
           },
         }}
