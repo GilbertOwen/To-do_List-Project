637da0c401670c6c207dc3222ccc89c8aeb25f2b
Gilbert Owen <88517849+GilbertOwen@users.noreply.github.com>
2024-05-06 13:14:42 +0700

    Created sidebar component (#14)
    * feat: created hero component for public page
    
    * feat: added custom media breakpoint for reponsive layout
    
    * refactor: removed unecessary style
    
    * feat: added overflow hidden to body
    
    * feat: created home page  and added hero component to home public page
    
    * feat: created sidebar component layout
    
    * style: changed the font color style and animation
    
    * refactor: changed the title and description about the app
    
    * feat: added Introduce component to public home page
    
    * fix: fixed bad spacing between component
    
    * feat: created Introduce component and its content
    
    * feat: added resource image for Introduce component content
    
    * fix build error
    
    * fix build error
    
    * fix build error
    
    * feat: added react icon depedencies
    
    * feat: added keyframes boing animation to tailwind config
    
    * feat: created Benefits component
    
    * feat: added Benefits component to public home page
    
    * feat: added scroll snap to public home page
    
    * style: redesign Hero component background and the other
    
    * feat: added scroll smooth behavior to html
    
    * style: restyling components on public home page
    
    * feat: added gsap depedencies
    
    * feat: added animation to hero component
    
    * feat: added framer-motion depedencies to the app
    
    * fix: fixed animation bugs
    
    * feat: added overflow hidden so the page wont overflow
    
    * feat: added animation to introduce component
    
    * fix: fixed animation bug
    
    * feat: added animation to benefits section
    
    * refactor: changed the animation delay and duration
    
    * feat: created sidebar component and its animation
    
    * refactor: changed the style of heading 1 greeting
    
    * feat: added sidebar component to the home page
    
    ---------
    
    Co-authored-by: Gilbert Owen <moptius@gmail.com>

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,10 +2,12 @@ import { Fragment } from "react";
 import Hero from "./_components/Hero";
 import Introduce from "./_components/Introduce";
 import Benefits from "./_components/Benefits";
+import Sidebar from "./_components/Sidebar";
 
 export default async function HomePage() {
   return (
       <div className="snap-y snap-mandatory overflow-y-scroll overflow-x-hidden h-screen">
+        <Sidebar></Sidebar>
         <Hero></Hero>
         <Introduce></Introduce>
         <Benefits></Benefits>
