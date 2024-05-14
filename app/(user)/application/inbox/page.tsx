aa63058a5ee0c0d8a0c659f09435170afa289ecb
Gilbert Owen <moptius@gmail.com>
2024-05-14 12:41:39 +0700

    feat: created inbox page and added inbox components

--- app/(user)/application/inbox/page.tsx
+++ app/(user)/application/inbox/page.tsx
@@ -1,15 +1,30 @@
+import { Suspense } from "react";
+import getTasks from "./_actions/getTasks";
 import InboxFilter from "./_components/InboxFilter";
 import InboxTask from "./_components/InboxTask";
+import Skeleton from "@mui/material/Skeleton";
+import Loading from "./_components/Loading";
+import Upbar from "./_components/Upbar";
 
-export default async function InboxPage() {
+export default async function InboxPage({
+  searchParams,
+}: {
+  searchParams?: {
+    sort?: string;
+  };
+}) {
+  let sort = searchParams?.sort || "nearest";
+  console.log(sort);
+  const tasks = await getTasks(sort);
+  // console.log(task)
   return (
     <div className="flex flex-col w-full min-h-screen">
-      <div className="py-6 border-b-2 flex flex-col items-center">
-        <h1 className="font-semibold text-5xl">Inbox</h1>
-      </div>
+      <Upbar></Upbar>
       <div className="grow w-[100%] sm:w-[70%] md:w-[60%] lg:w-[50%] xl:w-[40%] flex flex-col sm:border-x-2 mx-auto">
         <InboxFilter></InboxFilter>
-        <InboxTask></InboxTask>
+        <Suspense fallback={<Loading />}>
+          <InboxTask tasks={tasks}></InboxTask>
+        </Suspense>
       </div>
     </div>
   );
