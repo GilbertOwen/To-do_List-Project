e5fda1591003b4f1444d3f2a436348125d8805b6
Gilbert Owen <moptius@gmail.com>
2024-05-14 12:40:31 +0700

    feat: created component that shows all the task that is available

--- app/(user)/application/inbox/_components/InboxTask.tsx
+++ app/(user)/application/inbox/_components/InboxTask.tsx
@@ -1,32 +1,8 @@
-export default function InboxTask() {
-  const exampleTask = [
-    {
-      date: "24 May 2024",
-      tasks: [
-        {
-          name: "Eat",
-          priority: "high",
-        },
-        {
-          name: "Date",
-          priority: "medium",
-        },
-      ],
-    },
-    {
-      date: "26 May 2024",
-      tasks: [
-        {
-          name: "Sleep",
-          priority: "low",
-        },
-        {
-          name: "Call mom",
-          priority: "high",
-        },
-      ],
-    },
-  ];
+"use client"
+import type { Task } from "@prisma/client";
+import { motion } from "framer-motion";
+
+export default function InboxTask({ tasks }: { tasks: Task[] }) {
   const handlePriority = (priority: string) => {
     if (priority === "high") {
       return "bg-red-400";
@@ -37,40 +13,58 @@ export default function InboxTask() {
     }
   };
   return (
-    <div className="flex flex-col pb-4">
-      {exampleTask &&
-        exampleTask.map((task, index) => (
-          <div key={index}>
-            <h2
-              className={`p-4 ${
-                index === 0 ? "border-b-2" : "border-y-2"
-              } my-2 font-semibold `}
+    <div className="flex flex-col pb-4 gap-y-4">
+      {tasks.map((task, index) => (
+        <motion.div
+          initial={{
+            y: 100,
+            opacity: 0,
+          }}
+          animate={{
+            y: 0,
+            opacity: 1,
+          }}
+          exit={{ 
+            y:100,
+            opacity:1,
+            transition:{
+              duration:0.2,
+
+            }
+           }}
+          transition={{ 
+            duration:0.6,
+            ease:'easeInOut',
+            delay:0.8
+           }}
+          key={index}
+        >
+          <h2
+            className={`p-4 ${
+              index === 0 ? "border-b-2" : "border-y-2"
+            } my-2 font-semibold `}
+          >
+            {task.deadline_at.toDateString()}
+          </h2>
+          <div className="flex flex-col px-4 gap-y-2">
+            <div
+              className="w-full border-2 rounded-md p-2 cursor-grab"
+              key={index}
             >
-              {task.date}
-            </h2>
-            <div className="flex flex-col px-4 gap-y-2">
-                {/* Card List */}
-              {task.tasks &&
-                task.tasks.map((task,index) => (
-                  <div className="w-full border-2 rounded-md p-2 cursor-grab" key={index}>
-                    <div className="pb-1 border-b-2 mb-2 flex flex-row items-center gap-x-2">
-                      <span
-                        className={`w-[20px] h-[10px] ${handlePriority(
-                          task.priority
-                        )} block`}
-                      ></span>
-                      <h2 className="text-xl font-semibold">{task.name}</h2>
-                    </div>
-                    <p className="text-base font-medium">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Impedit, veritatis.
-                    </p>
-                    <small>See more ~</small>
-                  </div>
-                ))}
+              <div className="pb-1 border-b-2 mb-2 flex flex-row items-center gap-x-2">
+                <span
+                  className={`w-[20px] h-[10px] ${handlePriority(
+                    task.priority
+                  )} block`}
+                ></span>
+                <h2 className="text-xl font-semibold">{task.title}</h2>
+              </div>
+              <p className="text-base font-medium">{task.description}</p>
+              <small>See more ~</small>
             </div>
           </div>
-        ))}
+        </motion.div>
+      ))}
     </div>
   );
 }
