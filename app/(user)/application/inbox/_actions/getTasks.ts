57f314e87503244ae30de2b554ec0251f8074500
Gilbert Owen <moptius@gmail.com>
2024-05-14 13:15:45 +0700

    feat: adjust the logic so it can take the tasks by the user session id

--- app/(user)/application/inbox/_actions/getTasks.ts
+++ app/(user)/application/inbox/_actions/getTasks.ts
@@ -1,17 +1,21 @@
+import { authOptions } from "@/lib/auth-option";
 import prisma from "@/lib/db";
 import type { Task } from "@prisma/client";
+import { getServerSession } from "next-auth";
 
 export default async function getTasks(sortBy: string) {
   try {
     let tasks: Task[] = [];
+    const session:any = await getServerSession(authOptions)
+    console.log(session)
     if (sortBy === "aToZ") {
-      tasks = await prisma.task.findMany({ orderBy: { title: "asc" } });
+      tasks = await prisma.task.findMany({where:{userId:session?.id}, orderBy: { title: "asc" } });
     } else if (sortBy === "nearest") {
-      tasks = await prisma.task.findMany({ orderBy: { deadline_at: "asc" } });
+      tasks = await prisma.task.findMany({where:{userId:session?.id}, orderBy: { deadline_at: "asc" } });
     } else if (sortBy === "highest") {
-      tasks = await prisma.task.findMany({ orderBy: { priority: "asc" } });
+      tasks = await prisma.task.findMany({where:{userId:session?.id}, orderBy: { priority: "asc" } });
     } else if (sortBy === "lowest") {
-      tasks = await prisma.task.findMany({ orderBy: { priority: "desc" } });
+      tasks = await prisma.task.findMany({where:{userId:session?.id}, orderBy: { priority: "desc" } });
     }
 
     return tasks;
