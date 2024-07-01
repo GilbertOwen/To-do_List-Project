11095934855b4f399014194ef7ec79314230557e
Gilbert Owen <moptius@gmail.com>
2024-07-01 12:10:33 +0700

    refactor: adjusted the way to get user's data to firebase

--- app/(user)/application/layout.tsx
+++ app/(user)/application/layout.tsx
@@ -1,30 +1,34 @@
 import { getServerSession } from "next-auth";
 import { Fragment } from "react";
 import SidebarApp from "./_components/SidebarApp";
-import { authOptions } from "@/lib/auth-option";
-import prisma from "@/lib/db";
-import type { User } from "@prisma/client";
+import { authOptions, getCredential } from "@/lib/auth-option";
 import { signOut } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { Toaster } from "@/components/ui/toaster";
 
-export default async function ApplicationLayout({modal,children}: {modal:React.ReactNode,children:React.ReactNode}){
-    const session:any = await getServerSession(authOptions)
-    if(!session){
-        signOut()
-        redirect('/auth-to-do')
-    }
-    const user = await prisma.user.findUnique({where:{id:session.user.id, email:session.user.email}})
-    if(!user){
-        signOut()
-        redirect('/auth-to-do')
-    }
-    return (
-        <Fragment>
-            <Toaster />
-            <SidebarApp user={user as User}></SidebarApp>
-            {children}
-            {modal}
-        </Fragment>
-    )
+export default async function ApplicationLayout({
+  modal,
+  children,
+}: {
+  modal: React.ReactNode;
+  children: React.ReactNode;
+}) {
+  const session: any = await getServerSession(authOptions);
+  if (!session) {
+    signOut();
+    redirect("/auth-to-do");
+  }
+  const user = await getCredential(session.email);
+  if (!user) {
+    signOut();
+    redirect("/auth-to-do");
+  }
+  return (
+    <Fragment>
+      <Toaster />
+      <SidebarApp user={user}></SidebarApp>
+      {children}
+      {modal}
+    </Fragment>
+  );
 }
